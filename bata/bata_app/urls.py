71295ccc45df64634e035f46d4822ec8a1f20ee5
ankurjhaaa <kumarankur7258@gmail.com>
2026-03-11 01:36:34 +0530

    feat: Implement user account dashboard, order management, product details, wishlist, addresses, and checkout functionality.

--- bata/bata_app/urls.py
+++ bata/bata_app/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('cart/', views.cart, name='cart'),
     path('profile/', views.profile, name='profile'),
+    path('product/', views.product, name='product'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('orders/', views.orders, name='orders'),
+    path('order-details/', views.order_details, name='order_details'),
+    path('addresses/', views.addresses, name='addresses'),
+    path('wishlist/', views.wishlist, name='wishlist'),
 ]
--- bata/bata_app/views.py
+++ bata/bata_app/views.py
@@ -11,3 +11,21 @@ def cart(request):
 
 def profile(request):
     return render(request, 'public/profile.html')
+
+def product(request):
+    return render(request, 'public/product.html')
+
+def checkout(request):
+    return render(request, 'public/checkout.html')
+
+def orders(request):
+    return render(request, 'public/orders.html')
+
+def order_details(request):
+    return render(request, 'public/order_details.html')
+
+def addresses(request):
+    return render(request, 'public/addresses.html')
+
+def wishlist(request):
+    return render(request, 'public/wishlist.html')
